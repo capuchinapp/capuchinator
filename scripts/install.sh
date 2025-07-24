7b881cb3b26b31c1762d3ffe956720982ed972cf
Dmitrii Shcherbakov <dscherbakov@checkbox.ru>
2025-07-24 14:26:08 +0500

    Добавлены эмодзи в install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 # Проверяем, передан ли аргумент с версией
 if [ -z "$1" ]; then
-  echo "Usage: $0 <version>"
+  echo "💡 Usage: $0 <version>"
   echo "Example: $0 v0.1.0"
   exit 1
 fi
@@ -11,17 +11,17 @@ VERSION=$1
 TARBALL="capuchinator_${VERSION#v}_linux_amd64.tar.gz"
 URL="https://github.com/capuchinapp/capuchinator/releases/download/${VERSION}/capuchinator_${VERSION#v}_linux_amd64.tar.gz"
 
-echo "Download Capuchinator version ${VERSION}..."
+echo "⬇️ Download Capuchinator version ${VERSION}..."
 curl -L -o capuchinator.tar.gz "$URL"
 
-echo "Unpacking the archive..."
+echo "📦 Unpacking the archive..."
 tar -xzf capuchinator.tar.gz capuchinator
 
-echo "Setting execution rights..."
+echo "🔧 Setting execution rights..."
 chmod +x ./capuchinator
 
-echo "Deleting temporary files..."
+echo "🧹 Deleting temporary files..."
 rm capuchinator.tar.gz
 
-echo "Capuchinator ${VERSION} successfully installed!"
-echo "Launch: ./capuchinator"
+echo "✅ Capuchinator ${VERSION} successfully installed!"
+echo "🚀 Launch: ./capuchinator"
